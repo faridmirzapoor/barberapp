863cdfe0fc21fb9f886d87557c97f72d6b080a99
Farid_mr <itmirengineering@gmail.com>
2025-05-31 20:52:33 +0330

    editng header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div className='p-6 bg-amber-400'></div>
+      <div className='p-6 bg-amber-400'>
+        <p className='text-center color-white text-2xl'>hello</p>
+      </div>
       <div className='p-6 bg-blue-600'></div>
     </>
   )
