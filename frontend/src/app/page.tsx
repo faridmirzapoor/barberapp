71534e39014f252c65e5616b5d81063e3b2326f8
Farid_mr <itmirengineering@gmail.com>
2025-05-31 21:39:25 +0330

    sadc

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -59,6 +59,11 @@ const App = () => {
         <div className="sss"></div>
         <div className="sss"></div>
         <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
       </div>
     
     </>
