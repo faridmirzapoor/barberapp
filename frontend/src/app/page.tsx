8084a972921508be9ed82190bc345cf905280382
Farid_mr <itmirengineering@gmail.com>
2025-05-31 21:38:34 +0330

    saasd

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -55,6 +55,10 @@ const App = () => {
         <div className="sss"></div>
         <div className="sss"></div>
         <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
       </div>
     
     </>
