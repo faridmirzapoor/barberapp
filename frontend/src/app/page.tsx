23d2bf070e3700fc03f65a9487575edf4872b8a8
Farid_mr <itmirengineering@gmail.com>
2025-05-31 21:03:07 +0330

    sakdhxkz

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,19 @@ const App = () => {
       kkfvbjcx
       <p>kjbsfkdbv</p>
       <div className='p-6 bg-blue-600'></div>
+      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut saepe assumenda sapiente voluptatibus sit! Ipsum iste veniam porro aspernatur reprehenderit delectus reiciendis. Dignissimos quam ad distinctio incidunt, ipsam in recusandae?</div>
+      <div className="">
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+        <div className="sss"></div>
+      </div>
+    
     </>
+
   )
 }
 
